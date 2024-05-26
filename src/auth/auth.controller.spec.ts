45070435fb1796b63c5f25c36696de57d018a91e
Alan Duchêne <alan.duchene@zenika.com>
2024-05-26 08:51:59 +0200

    fix(auth-api): tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import {AuthService} from "./auth.service";
+import {UsersService} from "../users/users.service";
+import {UsersModule} from "../users/users.module";
+import {JwtModule} from "@nestjs/jwt";
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        UsersModule,
+        JwtModule.register({
+          global: true,
+          secret: 'temporary_secret_to_test',
+          signOptions: { expiresIn: '180s' },
+        })
+      ],
       controllers: [AuthController],
+      providers: [AuthService]
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import {UsersModule} from "../users/users.module";
+import {JwtModule} from "@nestjs/jwt";
+import {AuthController} from "./auth.controller";
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        UsersModule,
+        JwtModule.register({
+          global: true,
+          secret: 'temporary_secret_to_test',
+          signOptions: { expiresIn: '180s' },
+        })
+      ],
+      controllers: [AuthController],
+      providers: [AuthService]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
