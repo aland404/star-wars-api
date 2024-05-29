7d942ebe72bacc5f52b451d9987225b9e96808ab
Alan Duchêne <alan.duchene@zenika.com>
2024-05-29 07:36:58 +0200

    feat(war-api): update url endpoints

--- src/war/infrastructure/controller/war.controller.ts
+++ src/war/infrastructure/controller/war.controller.ts
@@ -18,23 +18,28 @@ export class WarController {
     @Get('/:warSlug')
     getAWarBySlug(@Param('warSlug') warSlug: string): WarPresenter | undefined {
         const war = this.warRepository.getAWarBySlug(warSlug)
-        if(!war) throw new HttpException('No war was found with the given slug', HttpStatus.NOT_FOUND)
+        if (!war) throw new HttpException('No war was found with the given slug', HttpStatus.NOT_FOUND)
 
         return toWarPresenter(war)
     }
 
-    @Get('/:warSlug/:battleSlug')
+    @Get('/:warSlug/summary')
+    getAWarSummaryBySlug(@Param('warSlug') warSlug: string): WarPresenter | undefined {
+        throw new HttpException('Not implemented yet', HttpStatus.NOT_FOUND)
+    }
+
+    @Get('/:warSlug/battles/:battleSlug')
     getABattleBySlug(@Param('warSlug') warSlug: string, @Param('battleSlug') battleSlug: string): BattlePresenter | undefined {
         const battle = this.warRepository.getABattleBySlug(warSlug, battleSlug)
-        if(!battle) throw new HttpException('No battle was found with the given slug', HttpStatus.NOT_FOUND)
+        if (!battle) throw new HttpException('No battle was found with the given slug', HttpStatus.NOT_FOUND)
 
         return toBattlePresenter(battle)
     }
 
-    @Get('/:warSlug/:battleSlug/summary')
+    @Get('/:warSlug/battles/:battleSlug/summary')
     getABattleSummary(@Param('warSlug') warSlug: string, @Param('battleSlug') battleSlug: string): BattleSummupPresenter | undefined {
         const battleSummup = this.warRepository.getABattleBySlug(warSlug, battleSlug)
-        if(!battleSummup) throw new HttpException('No battle was found with the given slug', HttpStatus.NOT_FOUND)
+        if (!battleSummup) throw new HttpException('No battle was found with the given slug', HttpStatus.NOT_FOUND)
 
         return toBattleSummupPresenter(battleSummup)
     }
